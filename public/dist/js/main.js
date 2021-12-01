ccf9c35f7cf294a173dcc9841a06a789006336b5
boutamente-abdessamad <boutamente.abdessamad@gmail.com>
2021-12-01 12:10:27 +0100

    Init project files

---
+++ public/dist/js/main.js
@@ -0,0 +1,25 @@
+/****************************************/
+/************ Main Scripts **************/
+/****************************************/
+
+$(document).ready(() => {
+
+
+    // start script 
+    try {
+
+        /* do your magic here*/
+
+
+
+    } catch (e) {
+        console.log('Error In script detected');
+        console.log('==========================');
+        console.error(e);
+    }
+    // end script 
+
+
+
+
+})
