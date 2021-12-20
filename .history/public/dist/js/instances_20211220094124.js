82e6c7b2316d0631889167f2eb9fc2ee95803c55
boutamente-abdessamad <boutamente.abdessamad@gmail.com>
2021-12-20 17:48:01 +0100

    add supliers pages

---
+++ .history/public/dist/js/instances_20211220094124.js
@@ -0,0 +1,367 @@
+/****************************************/
+/************ Instances Scripts **************/
+/****************************************/
+
+var initPlugins = function () {
+        // editable links 
+
+    if ($.fn.editable) {
+      // $.fn.editable.defaults.mode = 'inline';
+       $('.editable-link').editable({
+        //  toggle: 'manual'
+        mode: 'inline',
+      });
+    }
+
+    $('.ba-lg-circle-progress').circleProgress({});
+
+   
+
+      // select 
+
+    if ( $('.ba-select').length) {
+        $('.ba-select').selectpicker();
+    }
+    // editor wysiwyg
+    var editor = new FroalaEditor('#editor_wysiwyg',{
+        codeMirror: false,
+         tabSize: 4
+    })
+}
+
+$(document).ready(() => {
+
+
+    // start script 
+    try {
+
+      // init plugins 
+      initPlugins();
+    //   resize windows 
+    $(window).resize(() => {
+        // init plugins 
+         initPlugins();
+    });
+
+    // global datatabele 
+    if ($(".ba-datatable").length) {
+        let baDatatable = $(".ba-datatable").DataTable({
+            "paging": false,
+            "lengthChange": false,
+            "searching": false,
+            "ordering": true,
+            "info": false,
+            "responsive": true,
+            "autoWidth": false,
+            columnDefs: [{
+                orderable: false,
+                className: 'select-checkbox',
+                targets: 0,
+                checkboxes: {
+                    'selectRow': true
+                }
+            }],
+            select: {
+                style: 'multi',
+                selector: 'td:first-child'
+            },
+            dom: 'Bfrtip',
+            buttons: [
+                'selectAll',
+                'selectNone'
+            ],
+            order: [
+                [1, 'asc']
+            ]
+        });
+        baDatatable.on('select deselect', function (e, dt, type, indexes) {
+          if (type === 'row') {
+              // We may use dt instead of timesDataTable to have the freshest data.
+              if (dt.rows().count() === dt.rows({
+                      selected: true
+                  }).count()) {
+                  // Deselect all items button.
+                  $('#baDataTableCheckAllButton i').attr('class', 'fa fa-check-square');
+                  return;
+              }
+
+              if (dt.rows({
+                      selected: true
+                  }).count() === 0) {
+                  // Select all items button.
+                  $('#baDataTableCheckAllButton i').attr('class', 'fa fa-square');
+                  return;
+              }
+
+              // Deselect some items button.
+              $('#baDataTableCheckAllButton i').attr('class', 'fa fa-minus-square');
+          }
+      });
+      $('#baDataTableCheckAllButton').click(function (e) {
+          e.preventDefault();
+          if (baDatatable.rows({
+                  selected: true
+              }).count() > 0) {
+              baDatatable.rows().deselect();
+              return;
+          }
+
+          baDatatable.rows().select();
+      });
+    }
+    if ($(".ba-datatable-row-details").length) {
+        let baDatatable = $(".ba-datatable-row-details").DataTable({
+            "paging": false,
+            "lengthChange": false,
+            "searching": false,
+            "ordering": true,
+            "info": false,
+            "responsive": true,
+            "autoWidth": false,
+            columnDefs: [{
+                orderable: false,
+                className: 'select-checkbox',
+                targets: 0,
+                checkboxes: {
+                    'selectRow': true
+                }
+            }],
+            select: {
+                style: 'multi',
+                selector: 'td:first-child'
+            },
+            dom: 'Bfrtip',
+            buttons: [
+                'selectAll',
+                'selectNone'
+            ],
+            order: [
+                [1, 'asc']
+            ]
+        });
+        $('.ba-datatable-row-details tbody').on('click', 'td.dt-control', function () {
+          var template  = $('.invoice-form-template').html();
+          var tr = $(this).closest('tr');
+          var row = baDatatable.row( tr );
+  
+          if ( row.child.isShown() ) {
+              // This row is already open - close it
+              row.child.hide();
+              tr.removeClass('shown');
+          }
+          else {
+              // Open this row
+              // row.find('ts').style({'padding':'0'});
+              row.child( template,'ba-invoice-form-tr' ).show();
+              tr.addClass('shown');
+          }
+          initPlugins();
+      } );
+        baDatatable.on('select deselect', function (e, dt, type, indexes) {
+          if (type === 'row') {
+              // We may use dt instead of timesDataTable to have the freshest data.
+              if (dt.rows().count() === dt.rows({
+                      selected: true
+                  }).count()) {
+                  // Deselect all items button.
+                  $('#baDataTableCheckAllButton i').attr('class', 'fa fa-check-square');
+                  return;
+              }
+
+              if (dt.rows({
+                      selected: true
+                  }).count() === 0) {
+                  // Select all items button.
+                  $('#baDataTableCheckAllButton i').attr('class', 'fa fa-square');
+                  return;
+              }
+
+              // Deselect some items button.
+              $('#baDataTableCheckAllButton i').attr('class', 'fa fa-minus-square');
+          }
+      });
+      $('#baDataTableCheckAllButton').click(function (e) {
+          e.preventDefault();
+          if (baDatatable.rows({
+                  selected: true
+              }).count() > 0) {
+              baDatatable.rows().deselect();
+              return;
+          }
+
+          baDatatable.rows().select();
+      });
+    }
+    // daterangepicker
+    if ($('.date-range').length) {
+        $('.date-range').daterangepicker({
+            buttonClasses: "d-none"
+        })
+    }
+
+    // sliders 
+    // invoices-models-slider
+
+    let purchasesInvoicesSlider = $('.purchases-invoices-slider');
+    if (purchasesInvoicesSlider.length) {
+      purchasesInvoicesSlider.slick({
+        focusOnSelect: true,
+        slidesToShow: 4,
+        dots: false,
+        autoplay: false,
+        speed: 1000,
+        autoplaySpeed: 3000,
+        // arrows: false,
+        swipe: true,
+        prevArrow: $('.side-slider-actions .prev'),
+        nextArrow: $('.side-slider-actions .next'),
+        infinite: false,
+        // swipeToSlide: true,
+        // responsive: [{
+        //     breakpoint: 1025,
+        //     settings: {
+        //       slidesToShow: 3,
+        //     }
+        //   },
+        //   {
+        //     breakpoint: 769,
+        //     settings: {
+        //       slidesToShow: 2,
+        //     }
+        //   },
+        //   {
+        //     breakpoint: 480,
+        //     settings: {
+        //       centerMode: true,
+        //       slidesToShow: 1,
+        //     }
+        //   }
+        // ]
+      });
+      purchasesInvoicesSlider.show();
+    }
+    let suppliersSlider = $('.suppliers-slider');
+    if (suppliersSlider.length) {
+      suppliersSlider.slick({
+        focusOnSelect: true,
+        slidesToShow: 4,
+        dots: false,
+        autoplay: false,
+        speed: 1000,
+        autoplaySpeed: 3000,
+        arrows: true,
+        swipe: true,
+        prevArrow: $('.side-slider-actions .prev'),
+        nextArrow: $('.side-slider-actions .next'),
+        infinite: true,
+        // swipeToSlide: true,
+        // responsive: [{
+        //     breakpoint: 1025,
+        //     settings: {
+        //       slidesToShow: 3,
+        //     }
+        //   },
+        //   {
+        //     breakpoint: 769,
+        //     settings: {
+        //       slidesToShow: 2,
+        //     }
+        //   },
+        //   {
+        //     breakpoint: 480,
+        //     settings: {
+        //       centerMode: true,
+        //       slidesToShow: 1,
+        //     }
+        //   }
+        // ]
+      });
+      suppliersSlider.show();
+    }
+    let invoicesModelsSlider = $('.invoices-models-slider');
+    if (invoicesModelsSlider.length) {
+      invoicesModelsSlider.slick({
+        focusOnSelect: true,
+        slidesToShow: 2,
+        dots: false,
+        autoplay: false,
+        speed: 1000,
+        autoplaySpeed: 3000,
+        // arrows: false,
+        swipe: true,
+        prevArrow: $('.invoices-models-prev'),
+        nextArrow: $('.invoices-models-next'),
+        infinite: true,
+        // swipeToSlide: true,
+        // responsive: [{
+        //     breakpoint: 1025,
+        //     settings: {
+        //       slidesToShow: 3,
+        //     }
+        //   },
+        //   {
+        //     breakpoint: 769,
+        //     settings: {
+        //       slidesToShow: 2,
+        //     }
+        //   },
+        //   {
+        //     breakpoint: 480,
+        //     settings: {
+        //       centerMode: true,
+        //       slidesToShow: 1,
+        //     }
+        //   }
+        // ]
+      });
+    }
+     let baColorsSlider = $('.ba-colors-slider');
+    if (baColorsSlider.length) {
+      baColorsSlider.slick({
+        focusOnSelect: true,
+        slidesToShow: 7,
+        dots: false,
+        autoplay: false,
+        speed: 1000,
+        autoplaySpeed: 3000,
+        swipe: true,
+        infinite: true,
+        prevArrow: $('.colors-slider-prev'),
+        nextArrow: $('.colors-slider-next'),
+        // swipeToSlide: true,
+        // responsive: [{
+        //     breakpoint: 1025,
+        //     settings: {
+        //       slidesToShow: 3,
+        //     }
+        //   },
+        //   {
+        //     breakpoint: 769,
+        //     settings: {
+        //       slidesToShow: 2,
+        //     }
+        //   },
+        //   {
+        //     breakpoint: 480,
+        //     settings: {
+        //       centerMode: true,
+        //       slidesToShow: 1,
+        //     }
+        //   }
+        // ]
+      });
+    }
+        
+
+
+    } catch (e) {
+        console.log('Error In script detected');
+        console.log('==========================');
+        console.error(e);
+    }
+    // end script 
+
+
+
+
+})
