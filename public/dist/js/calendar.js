f37c9f41d0be27f7b2d0ce2eb2efcf483462a135
Boutament Abdessamad <boutamente.abdessamad@gmail.com>
2021-12-31 00:09:18 +0100

    change fullcalendar style

--- public/dist/js/calendar.js
+++ public/dist/js/calendar.js
@@ -10,12 +10,26 @@ $(document).ready(() => {
     try {
         // init full calendar
             var calendarEl = document.getElementById('ba_months_calendar');
-            console.log('====================================');
-            console.log(calendarEl);
-            console.log('====================================');
+          
             if (calendarEl) {
                 var calendar = new FullCalendar.Calendar(calendarEl, {
-                    initialView: 'dayGridMonth'
+                    initialView: 'dayGridMonth',
+                    dayCellDidMount  : function ({el}) {
+                      $(el).find('.fc-daygrid-day-frame').append($('.add-event-template').html());
+                    },
+                    //  eventDidMount   : function ({el}) {
+                    //   $(el).closest('.fc-daygrid-day-frame').append('<div class="ba-event-box ba-success-light-bg"> <span class="ba-left-dote-success ba-left-dote"></span><div class="event-content"><h3 class="ba-color-green">3<span>taches</span></h3><p>7.00</p></div></div>')
+                    // },
+                    eventContent: { html: $('.event-template').html() },
+                     events: [
+                        {
+                        title: 'My Event',
+                        start: '2021-12-06',
+                         backgroundColor : 'rgba(101, 208, 159, 0.11)',
+                         borderColor  : 'rgba(101, 208, 159, 0.11)'
+                        }
+                        // other events here
+                    ],
                 });
                 calendar.render();
             }
